3e1276c3cf4e1ed0cdac1f2f5b2f590b57ca1653
Abhijit Dasgupta <aikidasgupta@gmail.com>
2018-08-15 16:29:00 -0400

    Worked out Python connections

--- Examples.py
+++ Examples.py
@@ -1,1 +1,13 @@
 import pyodbc
+
+
+#cnxn = pyodbc.connect(
+#  r'DRIVER={ODBC DRIVER 17 for SQL Server};'
+#  r'SERVER=brkressql01.database.windows.net;'
+#  r'DATABASE=BRKRes01;
+#  r'UID=<your username>'
+#  r'PWD=<your password>'
+#)
+cnxn = pyodbc.connect('DSN={SQLBrookings};Database=BRKRes01;UID=<your username>;PWD=<your password>')
+cursor = cnxn.cursor()
+
